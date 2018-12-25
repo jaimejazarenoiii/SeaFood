a0fcfcb3fcb61f3ee4f7f13141026d4957e86431
Jaime Jazareno III <jaimejazarenoiii@Cjs-MacBook-Pro.local>
2018-12-26 00:12:51 +0800

    Initial setup

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -6,20 +6,52 @@
 //  Copyright © 2018 Jaime Jazareno III. All rights reserved.
 //
 
+import CoreML
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    super.viewDidLoad()
+    
+    imagePicker.delegate = self
+    imagePicker.sourceType = .camera
+    imagePicker.allowsEditing = false
+  }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+            let ciimage = CIImage(image: userPickedImage)
+            if #available(iOS 11.0, *) {
+                detect(ciimage!)
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @available(iOS 11.0, *)
+    func detect(_ image: CIImage) {
+        let model = try? VNCoreMLModel(for: Inceptionv3().model)
+        let request = VNCoreMLRequest(model: model!) { req, err in
+            let results = req.results as? [VNClassificationObservation]
+            self.label.text = results?.first?.identifier
+            self.label.adjustsFontSizeToFitWidth = true
+            self.label.isHidden = false
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        try! handler.perform([request])
     }
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        label.isHidden = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 
 }
 
